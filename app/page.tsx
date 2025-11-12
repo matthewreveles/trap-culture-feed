a1e12665b9664655b000b00c357fbc2695a04a96
matthewreveles <mattrebelious@gmail.com>
2025-11-12 15:12:29 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ fontFamily: "system-ui", textAlign: "center", padding: "5rem" }}>
+      <h1>Trap Culture Feed API</h1>
+      <p>Visit <code>/api/fb/photos</code> to see your Facebook feed JSON.</p>
+    </main>
+  );
+}
